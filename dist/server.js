82b6b10ff41bd757f1969433e7a2518cbb0d8d0a
Md Azad Hossain <sokalazad@gmail.com>
2025-06-26 02:35:17 +0200

    added app.listen event

--- dist/server.js
+++ dist/server.js
@@ -13,16 +13,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
 function server() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(config_1.default.database_url);
             console.log("Server is connected with the database");
-            app_1.default.listen(config_1.default.port, () => {
-                console.log(`library management server is running on ${config_1.default.port}`);
-            });
         }
         catch (error) {
             console.error(error);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import config from "./config";
+import app from "./app";
 
 async function server() {
   try {
     await mongoose.connect(config.database_url as string);
     console.log("Server is connected with the database");
+    app.listen(config.port, () => {
+      console.log("server is running.");
+    });
   } catch (error) {
     console.error(error);
   }
